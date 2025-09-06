cb5363ef514d4a8003e3b569152fe1c3ef12892c
Levi <levi.farias@aluno.ufca.edu.br>
2025-09-06 04:36:56 -0300

    Att do diálogo .json e melhoria parcial da interface de conversa

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-pergunstas_respostas=[
+perguntas_respostas=[
     {
-    "pergunta":"ttt",
-    "resposta": "vrggrgrvgr",
+    "pergunta":"",
+    "resposta": "",
     },{
     "pergunta":"",
     "resposta": "",
@@ -11,22 +11,27 @@ pergunstas_respostas=[
     }
 ]
 
-print("Olá! sou um bot de guia de turismo !")
-print("Escreva 'sair' para encerar o chat.")
+print("Olá! Sou o ChatBot Cariri, seu guia de turismo!", "\n" \
+"Que tal conhecer mais o Cariri? Tente me fazer uma pergunta ou escolha alguma das sugestões!","\n" \
+"(Encerre o chat com 'sair')")
 entrada = str(input("Digite olá para iniciar: "))
 if entrada.lower() == "ola":
     while True:
+        personalidade = int(input("Por padrão, a personalidade do chat é guia turístico"))
         pergunta=str(input("escreva sua pergunta:"))
         
         if (pergunta.lower()=="sair"):
-            print("Chat encerrado, até mais !")
+            print("Chat encerrado, até mais!")
             break
 
-        for i in range(len(pergunstas_respostas)):
+        for i in range(len(perguntas_respostas)):
             #modulo: lógica para identificar pergunta/resposta
-            if (pergunstas_respostas[i-1]["pergunta"] == pergunta): 
-                print(pergunstas_respostas[i-1]["resposta"])
-                
+            if (perguntas_respostas[i-1]["pergunta"] == pergunta): 
+                print(perguntas_respostas[i-1]["resposta"])
+            else:
+                print("Desculpe! Não consegui compreender sua pergunta.")
             #modulo: módulo caso a pergunta não esteja na base de dados
+elif entrada.lower() != "sair":
+    print("Inválido! O bot não foi iniciado.")
 else:
-    print("Inválido! O bot não foi iniciado")
+    print("Chat encerrado, até mais!")
